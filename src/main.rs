79d0ea14b45cb634e51c979cc39b8b87e882ec24
chip2n <andreas@arvidsson.io>
2020-12-12 08:43:42 +0100

    Switch to minifb crate

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,77 @@
-use log::error;
-use pixels::{Error, Pixels, SurfaceTexture};
-use winit::dpi::LogicalSize;
-use winit::{
-    event::Event,
-    event::VirtualKeyCode,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
-use winit_input_helper::WinitInputHelper;
+use bresenham::Bresenham;
+use minifb::{Key, Scale, Window, WindowOptions};
 
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 240;
+const WIDTH: usize = 240;
+const HEIGHT: usize = 160;
 
-fn main() -> Result<(), Error> {
-    let event_loop = EventLoop::new();
-    let mut input = WinitInputHelper::new();
-    let window = {
-        let size = LogicalSize::new(WIDTH as f32, HEIGHT as f32);
-        WindowBuilder::new()
-            .with_title("retro3d")
-            .with_inner_size(size)
-            .with_min_inner_size(size)
-            .with_max_inner_size(size)
-            .with_resizable(false)
-            .build(&event_loop)
-            .unwrap()
-    };
+type Point = (usize, usize);
 
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
-    };
+struct Player {
+    position: Point,
+}
 
-    event_loop.run(move |event, _, control_flow| {
-        if let Event::RedrawRequested(_) = event {
-            let frame = pixels.get_frame();
-            for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
-                let x = (i % WIDTH as usize) as i16;
-                let y = (i / WIDTH as usize) as i16;
+struct Line {
+    start: Point,
+    end: Point,
+}
 
-                let rgba = [0x5e, 0x48, 0xe8, 0xff];
+fn build_map() -> Vec<Line> {
+    vec![Line {
+        start: (40, 20),
+        end: (80, 20),
+    }]
+}
 
-                pixel.copy_from_slice(&rgba);
-            }
+fn main() {
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
-            let render_result = pixels
-                .render()
-                .map_err(|e| error!("pixels.render() failed: {}", e));
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions {
+            scale: Scale::X4,
+            ..WindowOptions::default()
+        },
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
 
-            if render_result.is_err() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    let player = Player { position: (50, 50) };
+    let map = build_map();
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 0xFF0000FF;
         }
 
-        if input.update(event) {
-            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-
-            if let Some(size) = input.window_resized() {
-                pixels.resize(size.width, size.height);
-            }
-
-            window.request_redraw();
+        for line in map.iter() {
+            draw_line(&mut buffer, &line);
         }
-    })
+
+        draw_player(&mut buffer, &player);
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
+}
+
+fn draw_player(buffer: &mut [u32], player: &Player) {
+    *pixel(buffer, player.position) = 0x00FF00;
+}
+
+fn draw_line(buffer: &mut [u32], line: &Line) {
+    let start = (line.start.0 as isize, line.start.1 as isize);
+    let end = (line.end.0 as isize, line.end.1 as isize);
+
+    let coords = Bresenham::new(start, end);
+    for (x, y) in coords {
+        *pixel(buffer, (x as usize, y as usize)) = 0x00FF00;
+    }
+}
+
+fn pixel(buffer: &mut [u32], point: Point) -> &mut u32 {
+    &mut buffer[(point.1 as usize * WIDTH) + point.0 as usize]
 }
